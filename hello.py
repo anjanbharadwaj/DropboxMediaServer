1655ee11f9970260716f3d20ac3d425b4c4a1116
hydefbla <anjanbharadwaj02@gmail.com>
2017-05-20 22:19:05 -0700

    Open Sourcing complete

--- hello.py
+++ hello.py
@@ -4,7 +4,7 @@ from flask import request
 from flask import redirect, url_for
 from werkzeug import secure_filename
 
-UPLOAD_FOLDER = "/home/pi/Documents/WebServer/dropbox/"
+UPLOAD_FOLDER = "/home/pi/Documents/WebServer/dropbox/" #enter your filepath for your dropbox folder here
 #from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
@@ -14,7 +14,7 @@ import os
 
 @app.route("/retrieve/<documentName>", methods=['GET'])
 def retrieve(documentName):
-    filename = "/home/pi/Documents/WebServer/dropbox/" + documentName
+    filename = UPLOAD_FOLDER + documentName
     b = os.path.exists(filename)
     if b:
         try:
@@ -25,15 +25,15 @@ def retrieve(documentName):
         return "File doesn't exist, sorry"
     #code for showing text
     #b = os.path.exists(filename)
-    # 
+    #
     #if b:
     #    with open(filename, 'r') as myfile:
     #        data=myfile.read().replace('\n', '')
     #    return data
     #else:
     #    return "File doesn't exist!"
-    
-    
+
+
 
 @app.route("/upload", methods=['POST'])
 def upload():
@@ -47,23 +47,22 @@ def upload():
     f.save(os.path.join(app.config['UPLOAD_FOLDER'], filename1))
     return 'file uploaded successfully'
 
-    
+
     #data = request.files[file]
     #error with this ^
     #return data
     #path = "/home/pi/Documents/WebServer/dropbox/" + documentName
     #if not os.path.exists(path):
     #    open(path, "wb")
-        
-            
+
+
         #with open(os.path.join("/home/pi/Documents/WebServer/dropbox/", documentName), 'wb') as temp_file:
             #temp_file.write()
-    
-    
+
+
 #comment the previous lines back in
     #return documentName
 
 if __name__ == "__main__":
     app.run()
 #
-
--- script.js
+++ script.js
@@ -5,32 +5,15 @@ var uploadButton = document.getElementById('upload-button');
 var btn = document.getElementById("download");
 $(document).ready(function() {
 	$("#download").click(function adownload() {
-		var url = "http://8a30a869.ngrok.io/retrieve/" + document.getElementById("filename").value;
+		var url = /* enter your ngrok url here */"http://8a30a869.ngrok.io/retrieve/" + document.getElementById("filename").value;
     		location.assign(url);
 	});
-	
+
 });
 
 function _(el){
 	return document.getElementById(el);
 }
-
-function uploadFile() {
-	var name = document.getElementById("filename1").value;
-	var file = _("file1").files[0];
-	var formdata = new FormData();
-	formdata.append(name,file);
-	$.ajax({
-       url : 'http://localhost:5000/upload/' + name,
-       type : 'POST',
-       data : formdata,
-       processData: false,  // tell jQuery not to process the data
-       contentType: false,  // tell jQuery not to set contentType
-       success : function(data) {
-           console.log(data);
-           alert(data);
-       }
-});
 
 }
 function progressHandler(event){
